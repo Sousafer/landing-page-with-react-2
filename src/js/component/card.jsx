0806bcf5a75128533b2574760b7c709de11b8bc4
Sousafer <fernando.ffsousa@gmail.com>
2022-07-02 17:49:01 +0000

    all

---
+++ src/js/component/card.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const card = (props) => {
+	return (
+		<div>
+			<div className="card">
+				<img
+					className="card-img-top"
+					src={props.img}
+					alt="Card image cap"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">{props.title}</h5>
+					<p className="card-text">{props.text}</p>
+					<a href="#" className="btn btn-primary">
+						{props.button}
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+card.PropTypes = {
+	img: PropTypes.string,
+	title: PropTypes.string,
+	text: PropTypes.string,
+	button: PropTypes.string,
+};
+
+export default card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
 import React from "react";
-
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Navbar />
+			<Jumbotron />
+			<div className="row">
+				<div className="col-md-3 m-4">
+					<Card
+						img="https://picsum.photos/200"
+						title="A title"
+						text="Some text"
+						button="Click me"
+					/>
+				</div>
+				<div className="col-md-3 m-4">
+					<Card
+						img="https://picsum.photos/200"
+						title="Another title"
+						text="Some more text"
+						button="Click me 2"
+					/>
+				</div>
+				<div className="col-md-3 m-4">
+					<Card
+						img="https://picsum.photos/200"
+						title="Lalalal"
+						text="Lorem ipsum"
+						button="Im a button"
+					/>
+				</div>
+				<div className="col-md-3 m-4">
+					<Card
+						img="https://picsum.photos/200"
+						title="Card title"
+						text="Some textttttttttt"
+						button="Now click"
+					/>
+				</div>
+			</div>
+			<footer className="footer mt-auto py-3">Copywrite 2022</footer>
 		</div>
 	);
 };
 
 export default Home;
+
